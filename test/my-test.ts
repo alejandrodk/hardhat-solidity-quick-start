d09c96b9f0611eea4bac68265ad080f7e666eb81
Alejandro Barrios <ottoabarriosp@hotmail.com>
2022-08-10 10:47:15 -0300

    Move contract deployment to beforeEach method

--- test/my-test.ts
+++ test/my-test.ts
@@ -1,31 +1,31 @@
 import { expect } from "chai";
 import hre, { ethers } from "hardhat"; // hardhat runtime environment
 import { time } from "@nomicfoundation/hardhat-network-helpers";
+import { Contract } from "ethers";
 
 describe("Lock", () => {
-  it("Should set the right unlockTime", async () => {
-    const LOCKED_AMOUNT = 1_000_000_000;
+  let lock: Contract;
+  let unlockTime: number;
+  let lockedAmount = 1_000_000;
+
+  beforeEach(async () => {
     const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
     // time.latest returns the timestamp of the last minted block
-    const unlockTime = (await time.latest()) + ONE_YEAR_IN_SECS;
+    unlockTime = (await time.latest()) + ONE_YEAR_IN_SECS;
 
     // deploy a lock contract where funds can be withdrawn
     // after 1 year
     const Lock = await hre.ethers.getContractFactory("Lock");
-    const lock = await Lock.deploy(unlockTime, { value: LOCKED_AMOUNT });
+    lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  })
+
+  it("Should set the right unlockTime", async () => {
     // all Functions on contract are Async, await the result to compare
     // values in a synchronous way
     expect(await lock.unlockTime()).to.equal(unlockTime);
   });
 
   it("Should revert with the right error if called to soon", async () => {
-    const LOCKED_AMOUNT = 1_000_000_000;
-    const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-    const unlockTime = (await time.latest()) + ONE_YEAR_IN_SECS;
-
-    const Lock = await hre.ethers.getContractFactory("Lock");
-    const lock = await Lock.deploy(unlockTime, { value: LOCKED_AMOUNT });
-
     // the whole assertion is async because it has yo wait until the transaction
     // is mined, so the "expect" call returns a promise
     await expect(lock.withdraw()).to.be.revertedWith("You can't withdraw yet");
@@ -33,13 +33,6 @@ describe("Lock", () => {
 
   it("Should transfer the funds to the owner", async () => {
     try {
-      const LOCKED_AMOUNT = 1_000_000_000;
-      const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-      const unlockTime = (await time.latest()) + ONE_YEAR_IN_SECS;
-
-      const Lock = await hre.ethers.getContractFactory("Lock");
-      const lock = await Lock.deploy(unlockTime, { value: LOCKED_AMOUNT });
-
       // mine a new block with the given timestamp
       await time.increaseTo(unlockTime);
       // should withdraw the funds
