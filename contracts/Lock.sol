d20eee0a2478e2aab0a79e516f5e75de2d08efbc
Alejandro Barrios <ottoabarriosp@hotmail.com>
2022-08-10 11:51:01 -0300

    add author

--- contracts/Lock.sol
+++ contracts/Lock.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.9;
 // Import this file to use console.log
 import "hardhat/console.sol";
 
+// Author: @Alejandrodk
 contract Lock {
     uint public unlockTime;
     address payable public owner;
