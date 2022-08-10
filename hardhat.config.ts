9683e124e2b15cec5f8e2bcc0157048dae976cfd
Alejandro Barrios <ottoabarriosp@hotmail.com>
2022-08-10 11:50:21 -0300

    Add networks config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,23 @@
+import dotenv from "dotenv";
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+dotenv.config();
+
+const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY as string;
+const GOERLI_PRIVATE_KEY = process.env.GOERLI_PRIVATE_KEY as string;
+
 const config: HardhatUserConfig = {
   solidity: "0.8.9",
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+  networks: {
+    goerli: {
+      url: `https://eth-goerli.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+      accounts: [GOERLI_PRIVATE_KEY],
+    },
+  },
 };
 
 export default config;
